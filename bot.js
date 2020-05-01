ff84bfe8a61a3feb1e7e80675fcd96748f84bc84
chasetoshh <47328560+chasetoshh@users.noreply.github.com>
2020-05-01 00:40:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.dXgK_m1TOfiLl9bKXyqp_ez_ZDZoxdKz);//BOT_TOKEN is the Client Secret
